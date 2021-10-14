f1f9fe48cf0188a7c2615f5fb4eab18943ef09d4
DimaMaslo88 <dmitrij-maslo@mail.ru>
2021-10-14 18:05:33 +0300

    freecodecamp

---
+++ 64.js
@@ -0,0 +1,9 @@
+var myDog = {
+// Only change code below this line
+    "name": "lazy bastard",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["cat", "chiken", "children"]
+
+// Only change code above this line
+};
