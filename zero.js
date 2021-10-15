65fd37b359258fd55a277ad8706072d4c61c3c47
DimaMaslo88 <dmitrij-maslo@mail.ru>
2021-10-15 22:20:11 +0300

    test js

--- zero.js
+++ zero.js
@@ -1,3 +1,7 @@
 let hobbiesId = "hobbies";
 let hobbiesEl = document.getElementById(hobbiesId);
 hobbiesEl.value = "run";
+
+let arr = ['I', 'Learn', 'JavaScript'];
+arr.splice(0,2, 'We', 'Like');
+alert( arr );
