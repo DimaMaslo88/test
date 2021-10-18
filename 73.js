f215fb3cbbaab1d05162fbbb2f97c2e06622aefa
DimaMaslo88 <dmitrij-maslo@mail.ru>
2021-10-18 18:46:02 +0300

    freecodecamp

---
+++ 73.js
@@ -0,0 +1,24 @@
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Dima Maslo",
+        "title": "JS Songs",
+        "release_year": 2021,
+        "formats": [
+            "DVD",
+            "Blue_Ray",
+            "USB"
+        ],
+
+    }
+    ]
